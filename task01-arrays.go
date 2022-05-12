4985e9eb9d0a2e9c60933a61007d09b2f3676922
zhnbvsst10 <63463825+zhnbvsst10@users.noreply.github.com>
2022-05-12 18:12:12 +0600

    Update task01-arrays.go

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,11 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
-	return
+	var sum float32 = 0.0
+	for i := 0; i < len(input); i++ {
+		sum = sum + input[i]
+	}
+	var length float32 = float32(len(input))
+	var ans float32 = sum / length
+	return ans
 }
