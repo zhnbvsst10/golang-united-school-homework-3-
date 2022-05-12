e35d226744cb04d6be88dd6e2ecd0e0836297c8e
zhnbvsst10 <63463825+zhnbvsst10@users.noreply.github.com>
2022-05-12 18:51:11 +0600

    Update task03-map.go

--- task03-map.go
+++ task03-map.go
@@ -7,11 +7,18 @@ import (
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
 	keys := make([]int, len(input))
+	output := make([]string, len(input))
 	i := 0
 	for k := range input {
 		keys[i] = k
+		//fmt.Println(k)
 		i++
 	}
+	sort.Ints(keys)
+	for k := range keys {
+		output[k] = input[keys[k]]
+	}
 
-	return sort.Ints(keys)
+	return output
 }
+
